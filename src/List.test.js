5672af5b7cbf4fee94a80de9aec622a7aff6dc46
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-12-11 13:00:34 -0400

    Add test for List component

---
+++ src/List.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('List component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List cards={[]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List cards={[]}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
